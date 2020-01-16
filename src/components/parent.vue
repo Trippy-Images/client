892d94b29d759c06b3570e2c622904285723d8f0
Okka Linardi <okka.linardi@gmail.com>
2020-01-16 20:01:19 +0700

    Finished Parent Vue

--- src/components/parent.vue
+++ src/components/parent.vue
@@ -3,9 +3,17 @@
     <NavBar></NavBar>
     <b-container fluid>
     <b-row class="text-center justify-content-md-center">
-        <b-col cols="8" id="loginPart" >
-            <b-row>
-
+        <b-col cols="8" id="loginPart" v-if="!isLogin">
+            <b-row class="text-center justify-content-md-center">
+                <label for="email">email:</label>
+                <input type="text" v-model="email">
+            </b-row>
+            <b-row class="text-center justify-content-md-center">
+                <label for="password">password:</label>
+                <input type="password" v-model="password">
+            </b-row>
+            <b-row class="text-center justify-content-md-center">
+                <button>Login</button>
             </b-row>
         </b-col>
     </b-row>
@@ -17,13 +25,13 @@
             <button v-on:click="againButton">Again</button>
         </b-col>
     </b-row>
-    <b-row class="text-center justify-content-md-center">
-        <Kenny></Kenny>
-        <Kenny></Kenny>
+    <b-row class="text-center justify-content-md-center" v-if="isLogin">
+        <Upload v-on:imageSelected="addImage($event, 0)"></Upload>
+        <Upload v-on:imageSelected="addImage($event, 1)"></Upload>
     </b-row>
     <b-row class="text-center justify-content-md-center">
         <b-col>
-            <button v-on:click="ResultButton" v-if="resultPart==null">Result</button>
+            <button v-on:click="ResultButton" v-if="isLogin">Result</button>
         </b-col>
     </b-row>
 </b-container>
@@ -32,26 +40,71 @@
 
 <script>
 import NavBar from './NavBar'
-import Kenny from './Kenny'
+import Upload from './Upload'
+import axios from 'axios'
 
 export default {
     name: 'parent',
     components: {
         NavBar,
-        Kenny
+        Upload
+    },
+    created(){
+        if(localStorage.getItem('token')){
+            this.isLogin = true
+        }else{
+            this.isLogin = true
+        }
     },
     data(){
         return {
-            resultPart : null
+            resultPart : null,
+            images: [],
+            style: null,
+            content: null,
+            isLogin: false,
+            email: '',
+            password:''
         }
     },
     methods:{
         ResultButton(){
-            this.resultPart = 'https://i.kym-cdn.com/photos/images/original/001/582/190/6d2.png'
-            // ini nanti get ke server
+            axios({
+                method: 'post',
+                url: 'http://localhost:3000',
+                data: {
+                    images: this.images
+                },
+                headers:{
+                    token: localStorage.getItem('token')
+                }
+            })
+            .then(({ data })=>{
+                this.resultPart = data.result
+                this.style = data.style
+                this.content = data.content
+            })
         },
         againButton(){
             this.resultPart = null
+            this.images = []
+
+        },
+        addImage(file, index){
+            this.images[index] = file
+        },
+        signIn(){
+            axios({
+                method: 'post',
+                url: 'http://localhost:3000',
+                data:{
+                    email: this.email,
+                    password: this.password
+                }
+            })
+            .then(({ data })=>{
+                this.isLogin = true
+            })
         }
     }
 }
